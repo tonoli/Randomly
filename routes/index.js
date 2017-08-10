5ef2547446523f6d828436172d3e2d8559baea8f
Ignacio <ignacio@Ignacios-MacBook-Pro.local>
2017-08-11 01:09:39 +0200

    Text, buttons and CSS ready 🎨

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res){
   res.render('index');
 })
 
+// Get Random image page
+
+router.get('/randomly', function(req, res){
+  res.render('randomly');
+})
+
 module.exports = router;
