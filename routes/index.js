f629055e599e878d008d19fd17647b46239e4a37
Ignacio <itonoli42@gmail.com>
2017-08-13 03:15:49 +0200

    fileFilter correction and file name

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,51 @@ var router = express.Router();
 var multer = require('multer');
 var path = require('path');
 
-// Multer config + Storage
-const imageFilter = function (req, file, callback) {
+// Accept images (jpeg, png, gif) only
+const imageFilter = function (req, file, cb) {
     // accept image only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return callback(new Error('Only image files are allowed!'), false);
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/)) {
+        return cb(console.log('Only image files are allowed!'), false);
     }
-    callback(null, true);
+    cb(null, true);
 };
 
-const upload = multer({
-  dest: './uploads',
-  fileFilter : imageFilter,
-});
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploads')
+	},
+	filename: function(req, file, callback) {
+		callback(null, 'images_' + Date.now() + path.extname(file.originalname))
+	}
+})
 
-router.post('/', upload.single('image'), function (req, res){
+router.post('/', function(req, res) {
+	var upload = multer({
+		storage: storage,
+		fileFilter: imageFilter,
+	}).single('image');
+	upload(req, res, function(err) {
     try {
         console.log(req.body);
         console.log(req.file);
-        res.send({filename : 'image-'+ Date.now() + path.extname(file.originalname)}).end('File is uploaded');
+        res.sendStatus(204).end('File is uploaded');
+    } catch (err) {
+        console.log('ERROR upload');
+        res.sendStatus(404);
+    }
+	})
+})
+/*
+    try {
+        console.log(req.body);
+        console.log(req.file);
+        res.sendStatus(204).end('File is uploaded');
     } catch (err) {
         console.log('ERROR upload');
         res.sendStatus(404);
     }
 })
-
+*/
 router.get('/images', function (req, res){
     try {
         const col = loadCollection(COLLECTION_NAME, db);
